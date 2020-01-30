7c075472de7382ed1cd83df48267a68bc7b47d9a
Arvind Draffen <arvinddraffen@gmail.com>
2020-01-29 20:36:02 -0600

    Added GUI elements for selecting groups/chats and preliminary interfacing with GroupMeStats.py code

--- gui.py
+++ gui.py
@@ -1,15 +1,8 @@
-from PyQt5.QtWidgets import QLabel
-from PyQt5.QtWidgets import QMainWindow
-from PyQt5.QtWidgets import QApplication
-from PyQt5.QtWidgets import QHBoxLayout, QVBoxLayout
-from PyQt5.QtWidgets import QWidget
-from PyQt5.QtWidgets import QTabWidget
-from PyQt5.QtWidgets import QListWidget
-from PyQt5.QtWidgets import QPushButton
+import PyQt5.QtWidgets
 import sys
 import GroupMeStats
 
-class GUI(QMainWindow):
+class GUI(PyQt5.QtWidgets.QMainWindow):
     def __init__(self):
         super().__init__()
         self.init_gui()
@@ -24,17 +17,19 @@ class GUI(QMainWindow):
     def init_tab_widget(self):
         self.tab_widget = TabWidget(self)
 
-class TabWidget(QWidget):
+
+class TabWidget(PyQt5.QtWidgets.QWidget):
     def __init__(self, parent):
         super(TabWidget, self).__init__(parent)
-        self.layout = QVBoxLayout(self)
+        self.layout = PyQt5.QtWidgets.QVBoxLayout(self)
+        self.users = set()
         self.setup()
 
     def setup(self):
-        self.tab_widget = QTabWidget()
-        self.group_tab = QWidget()
-        self.chat_tab = QWidget()
-        self.settings_tab = QWidget()
+        self.tab_widget = PyQt5.QtWidgets.QTabWidget()
+        self.group_tab = PyQt5.QtWidgets.QWidget()
+        self.chat_tab = PyQt5.QtWidgets.QWidget()
+        self.settings_tab = PyQt5.QtWidgets.QWidget()
 
         self.setup_group_tab()
         self.setup_chat_tab()
@@ -47,41 +42,133 @@ class TabWidget(QWidget):
         self.setLayout(self.layout)
 
     def setup_group_tab(self):
-        self.load_groups_btn = QPushButton('Load Groups', self)
-        self.group_tab_layout = QVBoxLayout(self.group_tab)
-        self.group_tab_sublayout = QHBoxLayout(self.group_tab)
+        self.load_groups_btn = PyQt5.QtWidgets.QPushButton('Load Groups', self)
+        self.load_groups_btn.clicked.connect(self.setup_group_table_widget)
+        self.group_tab_layout = PyQt5.QtWidgets.QGridLayout(self.group_tab)
+        self.group_tab_sublayout = PyQt5.QtWidgets.QHBoxLayout()
         self.group_tab_sublayout.addStretch()
         self.group_tab_sublayout.addWidget(self.load_groups_btn)
         self.group_tab_sublayout.addStretch()
-        self.group_tab_layout.addLayout(self.group_tab_sublayout)
-        self.group_tab_layout.addStretch()
+        self.group_tab_layout.addLayout(self.group_tab_sublayout, 0, 0)
         self.group_tab.setLayout(self.group_tab_layout)
 
     def setup_chat_tab(self):
-        self.load_chats_btn = QPushButton('Load Chats', self)
-        self.chat_tab_layout = QVBoxLayout(self.chat_tab)
-        self.chat_tab_sublayout = QHBoxLayout(self.chat_tab)
+        self.load_chats_btn = PyQt5.QtWidgets.QPushButton('Load Chats', self)
+        self.chat_tab_layout = PyQt5.QtWidgets.QGridLayout(self.chat_tab)
+        self.load_chats_btn.clicked.connect(self.setup_chat_table_widget)
+        self.chat_tab_sublayout = PyQt5.QtWidgets.QHBoxLayout()
         self.chat_tab_sublayout.addStretch()
         self.chat_tab_sublayout.addWidget(self.load_chats_btn)
         self.chat_tab_sublayout.addStretch()
-        self.chat_tab_layout.addLayout(self.chat_tab_sublayout)
-        self.chat_tab_layout.addStretch()
+        self.chat_tab_layout.addLayout(self.chat_tab_sublayout, 0, 0)
         self.chat_tab.setLayout(self.chat_tab_layout)
     
     def setup_settings_tab(self):
-        print("hi")
+        print("This is the settings tab")
+    
+    def get_groups(self):
+        if not GroupMeStats.set_token(None, from_gui=True):
+            sys.exit()
+        return GroupMeStats.get_groups(self.users, suppress_print=True)
 
-class ListWidget(QWidget):
-    def __init__(self, parent):
-        super(ListWidget, self).__init__(parent)
-        self.item = ""
-    def selected(self, item):
-        self.item = item
-    def get_selected_item(self):
-        return self.item
+    def get_chats(self):
+        if not GroupMeStats.set_token(None, from_gui=True):
+            sys.exit()
+        return GroupMeStats.get_chats(self.users, suppress_print=True)
+    
+    def setup_group_table_widget(self):
+        self.groups = self.get_groups()
+        self.group_table_widget = PyQt5.QtWidgets.QTableWidget(len(self.groups[1]), 3, self)
+        self.group_table_widget.setHorizontalHeaderLabels(['Group', 'Message Count', 'Group ID'])
+        i = 0
+        for group in self.groups[1]:
+            self.group_table_widget.setItem(i, 0, PyQt5.QtWidgets.QTableWidgetItem(self.groups[2][group][0]))
+            self.group_table_widget.setItem(i, 1, PyQt5.QtWidgets.QTableWidgetItem(str(self.groups[2][group][1])))
+            self.group_table_widget.setItem(i, 2, PyQt5.QtWidgets.QTableWidgetItem(str(group)))
+            i += 1
+        self.group_table_widget.itemSelectionChanged.connect(self.group_table_widget_update_selection)
+        self.group_table_widget.setSelectionBehavior(PyQt5.QtWidgets.QAbstractItemView.SelectRows)
+        self.group_table_widget.setEditTriggers(PyQt5.QtWidgets.QTableWidget.NoEditTriggers)
+        self.group_tab_layout.addWidget(self.group_table_widget, 1, 0)
+        self.group_analysis_all_btn = PyQt5.QtWidgets.QPushButton('Analyze All', self)
+        self.group_analysis_selected_btn = PyQt5.QtWidgets.QPushButton('Analyze Selected', self)
+        self.group_analysis_all_btn.clicked.connect(self.retrieve_group_messages)
+        self.group_analysis_selected_btn.clicked.connect(self.retrieve_group_messages)
+        group_tab_sublayout_2 = PyQt5.QtWidgets.QHBoxLayout()
+        group_tab_sublayout_2.addStretch()
+        group_tab_sublayout_2.addWidget(self.group_analysis_all_btn)
+        group_tab_sublayout_2.addStretch()
+        group_tab_sublayout_2.addWidget(self.group_analysis_selected_btn)
+        group_tab_sublayout_2.addStretch()
+        self.group_tab_layout.addLayout(group_tab_sublayout_2, 2, 0)
+    
+    def setup_chat_table_widget(self):
+        self.chats = self.get_chats()
+        self.chat_table_widget = PyQt5.QtWidgets.QTableWidget(len(self.chats[1]), 3, self)
+        self.chat_table_widget.setHorizontalHeaderLabels(['Chat', 'Message Count', 'Chat ID'])
+        i = 0
+        for chat in self.chats[1]:
+            self.chat_table_widget.setItem(i, 0, PyQt5.QtWidgets.QTableWidgetItem(self.chats[2][chat][0]))
+            self.chat_table_widget.setItem(i, 1, PyQt5.QtWidgets.QTableWidgetItem(str(self.chats[2][chat][1])))
+            self.chat_table_widget.setItem(i, 2, PyQt5.QtWidgets.QTableWidgetItem(str(chat)))
+            i += 1
+        self.chat_table_widget.itemSelectionChanged.connect(self.chat_table_widget_update_selection)
+        self.chat_table_widget.setSelectionBehavior(PyQt5.QtWidgets.QAbstractItemView.SelectRows)
+        self.chat_table_widget.setEditTriggers(PyQt5.QtWidgets.QTableWidget.NoEditTriggers)
+        self.chat_tab_layout.addWidget(self.chat_table_widget, 1, 0)
+        self.chat_analysis_all_btn = PyQt5.QtWidgets.QPushButton('Analyze All', self)
+        self.chat_analysis_selected_btn = PyQt5.QtWidgets.QPushButton('Analyze Selected', self)
+        self.chat_analysis_all_btn.clicked.connect(self.retrieve_chat_messages)
+        self.chat_analysis_selected_btn.clicked.connect(self.retrieve_chat_messages)
+        chat_tab_sublayout2 = PyQt5.QtWidgets.QHBoxLayout()
+        chat_tab_sublayout2.addStretch()
+        chat_tab_sublayout2.addWidget(self.chat_analysis_all_btn)
+        chat_tab_sublayout2.addStretch()
+        chat_tab_sublayout2.addWidget(self.chat_analysis_selected_btn)
+        chat_tab_sublayout2.addStretch()
+        self.chat_tab_layout.addLayout(chat_tab_sublayout2, 2, 0)
 
+    def group_table_widget_update_selection(self):
+        selected_groups = self.group_table_widget.selectedItems()
+        self.selected_group_ids = []
+        for group in selected_groups:
+            if group.text() in self.groups[1]:         # we only want the Group ID for retrieving messages
+                self.selected_group_ids.append(group.text())
+    
+    def chat_table_widget_update_selection(self):
+        selected_chats = self.chat_table_widget.selectedItems()
+        self.selected_chat_ids = []
+        for chat in selected_chats:
+            if chat.text() in self.chats[1]:
+                self.selected_chat_ids.append(chat.text())
+    
+    def retrieve_group_messages(self):
+        if self.group_analysis_all_btn == self.sender():
+            self.group_analysis_results = GroupMeStats.retrieve_group_messages(self.groups[1], self.groups[2])
+        elif self.group_analysis_selected_btn == self.sender():
+            try:
+                if len(self.selected_group_ids) > 0:
+                    self.group_analysis_results = GroupMeStats.retrieve_group_messages(self.selected_group_ids, self.groups[2])
+                    print(self.selected_group_ids)
+                else:
+                    print("Unable to begin")        # unexpected error dialog should go here
+            except AttributeError:                  # must select a row dialog should go here
+                print("Not yet created")
+    
+    def retrieve_chat_messages(self):
+        if self.chat_analysis_all_btn == self.sender():
+            self.chat_analysis_results = GroupMeStats.retrieve_chat_messages(self.chats[1], self.chats[2])
+        elif self.chat_analysis_selected_btn == self.sender():
+            try:
+                if len(self.selected_chat_ids) > 0:
+                    self.chat_analysis_results = GroupMeStats.retrieve_chat_messages(self.selected_chat_ids, self.chats[2])
+                    print(self.selected_chat_ids)
+                else:
+                    print("Unable to begin")        # unexpected error dialog here
+            except AttributeError:                  # must select a row dialog here
+                print("Not yet created")
 
 if __name__ == '__main__':
-    app = QApplication(sys.argv)
+    app = PyQt5.QtWidgets.QApplication(sys.argv)
     gui = GUI()
     sys.exit(app.exec_())
