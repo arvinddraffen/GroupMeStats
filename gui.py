54ef4f28a631f0b7d38ebdd86ed36f30a7a0273b
Arvind Draffen <arvinddraffen@gmail.com>
2022-08-01 08:59:13 -0500

    Added basic graphing utilities for statistics.

--- gui.py
+++ gui.py
@@ -1,10 +1,12 @@
-import PyQt5.QtWidgets
+from nntplib import GroupInfo
+from tkinter import Scrollbar
+from PyQt5 import QtCore, QtWidgets, QtGui
 import sys
 import GroupMeStats
 from pyqtgraph import PlotWidget, plot, AxisItem
 import pyqtgraph
 
-class GUI(PyQt5.QtWidgets.QMainWindow):
+class GUI(QtWidgets.QMainWindow):
     def __init__(self):
         super().__init__()
         self.init_gui()
@@ -20,10 +22,16 @@ class GUI(PyQt5.QtWidgets.QMainWindow):
         self.tab_widget = TabWidget(self)
 
 class GraphManager(pyqtgraph.PlotWidget):
-    def __init__(self, parent=None, background='default', plotItem=None, **kargs):
+    def __init__(self, title="", xLabel="", yLabel="", parent=None, background='default', plotItem=None, **kargs):
         super().__init__(parent, background, plotItem, **kargs)
         self.stringaxis = pyqtgraph.AxisItem(orientation="bottom")
         self.setBackground('w')
+        if title:
+            self.title = title
+        if xLabel:
+            self.xLabel = xLabel
+        if yLabel:
+            self.yLabel = yLabel
     
     def set_x_axis(self, values: list):
         self.x_axis = values
@@ -39,24 +47,25 @@ class GraphManager(pyqtgraph.PlotWidget):
     def plot_graph(self):
         self.stringaxis.setTicks([self.x_axis_dict.items()])
         self.setAxisItems(axisItems = {'bottom': self.stringaxis})
-        # self.plot(list(self.x_axis_dict.keys()), self.y_axis)
         bargraph = pyqtgraph.BarGraphItem(x = list(self.x_axis_dict.keys()), height = self.y_axis, width = 1.0)
         self.addItem(bargraph)
+        if self.title:
+            self.setTitle(self.title)
 
 
-class TabWidget(PyQt5.QtWidgets.QWidget):
+class TabWidget(QtWidgets.QWidget):
     def __init__(self, parent):
         super(TabWidget, self).__init__(parent)
-        self.layout = PyQt5.QtWidgets.QVBoxLayout(self)
+        self.layout = QtWidgets.QVBoxLayout(self)
         self.users = set()
         self.setup()
 
     def setup(self):
-        self.tab_widget = PyQt5.QtWidgets.QTabWidget()
-        self.group_tab = PyQt5.QtWidgets.QWidget()
-        self.chat_tab = PyQt5.QtWidgets.QWidget()
-        self.plots_tab = PyQt5.QtWidgets.QWidget()
-        self.settings_tab = PyQt5.QtWidgets.QWidget()
+        self.tab_widget = QtWidgets.QTabWidget()
+        self.group_tab = QtWidgets.QWidget()
+        self.chat_tab = QtWidgets.QWidget()
+        self.plots_tab = QtWidgets.QScrollArea()
+        self.settings_tab = QtWidgets.QWidget()
 
         self.setup_group_tab()
         self.setup_chat_tab()
@@ -71,10 +80,10 @@ class TabWidget(PyQt5.QtWidgets.QWidget):
         self.setLayout(self.layout)
 
     def setup_group_tab(self):
-        self.load_groups_btn = PyQt5.QtWidgets.QPushButton('Load Groups', self)
+        self.load_groups_btn = QtWidgets.QPushButton('Load Groups', self)
         self.load_groups_btn.clicked.connect(self.setup_group_table_widget)
-        self.group_tab_layout = PyQt5.QtWidgets.QGridLayout(self.group_tab)
-        self.group_tab_sublayout = PyQt5.QtWidgets.QHBoxLayout()
+        self.group_tab_layout = QtWidgets.QGridLayout(self.group_tab)
+        self.group_tab_sublayout = QtWidgets.QHBoxLayout()
         self.group_tab_sublayout.addStretch()
         self.group_tab_sublayout.addWidget(self.load_groups_btn)
         self.group_tab_sublayout.addStretch()
@@ -82,10 +91,10 @@ class TabWidget(PyQt5.QtWidgets.QWidget):
         self.group_tab.setLayout(self.group_tab_layout)
 
     def setup_chat_tab(self):
-        self.load_chats_btn = PyQt5.QtWidgets.QPushButton('Load Chats', self)
-        self.chat_tab_layout = PyQt5.QtWidgets.QGridLayout(self.chat_tab)
+        self.load_chats_btn = QtWidgets.QPushButton('Load Chats', self)
+        self.chat_tab_layout = QtWidgets.QGridLayout(self.chat_tab)
         self.load_chats_btn.clicked.connect(self.setup_chat_table_widget)
-        self.chat_tab_sublayout = PyQt5.QtWidgets.QHBoxLayout()
+        self.chat_tab_sublayout = QtWidgets.QHBoxLayout()
         self.chat_tab_sublayout.addStretch()
         self.chat_tab_sublayout.addWidget(self.load_chats_btn)
         self.chat_tab_sublayout.addStretch()
@@ -97,26 +106,28 @@ class TabWidget(PyQt5.QtWidgets.QWidget):
         self.generate_plots_layout()
 
     def generate_plots_layout(self):
-        self.plots_tab_layout = PyQt5.QtWidgets.QGridLayout(self.plots_tab)
+        self.plots_tab_layout = QtWidgets.QGridLayout(self.plots_tab)
         # messages sent
-        self.messages_sent_graph = GraphManager(parent=self.plots_tab)
+        self.messages_sent_graph = GraphManager(parent=self.plots_tab, title="Messages Sent Per User", xLabel="User", yLabel="Message Count")
         self.plots_tab_layout.addWidget(self.messages_sent_graph, 0, 0)
         # likes received
-        self.likes_received_graph = GraphManager(parent=self.plots_tab)
-        self.plots_tab_layout.addWidget(self.likes_received_graph, 0, 1)
+        self.likes_received_graph = GraphManager(parent=self.plots_tab, title="Likes Received Per User", xLabel="User", yLabel="Likes Received Count")
+        self.plots_tab_layout.addWidget(self.likes_received_graph, 1, 0)
         # likes given
-        self.likes_given_graph = GraphManager(parent=self.plots_tab)
-        self.plots_tab_layout.addWidget(self.likes_given_graph, 0, 2)
+        self.likes_given_graph = GraphManager(parent=self.plots_tab, title="Likes Given Per User", xLabel="User", yLabel="Likes Given Count")
+        self.plots_tab_layout.addWidget(self.likes_given_graph, 2, 0)
         # self likes
-        self.self_likes_graph = GraphManager(parent=self.plots_tab)
-        self.plots_tab_layout.addWidget(self.self_likes_graph, 1, 0)
+        self.self_likes_graph = GraphManager(parent=self.plots_tab, title="Self Likes Per User", xLabel="User", yLabel="Self Likes Count")
+        self.plots_tab_layout.addWidget(self.self_likes_graph, 3, 0)
         # words sent
-        self.words_sent_graph = GraphManager(parent=self.plots_tab)
-        self.plots_tab_layout.addWidget(self.words_sent_graph, 1, 1)
+        self.words_sent_graph = GraphManager(parent=self.plots_tab, title="Words Sent Per User", xLabel="User", yLabel="Words Sent Count")
+        self.plots_tab_layout.addWidget(self.words_sent_graph, 4, 0)
         # images sent
-        self.images_sent_graph = GraphManager(parent=self.plots_tab)
-        self.plots_tab_layout.addWidget(self.images_sent_graph, 1, 2)
+        self.images_sent_graph = GraphManager(parent=self.plots_tab, title="Images Sent Per User", xLabel="User", yLabel="Images Sent Count")
+        self.plots_tab_layout.addWidget(self.images_sent_graph, 5, 0)
+        self.plots_tab.setWidgetResizable(False)
         self.plots_tab.setLayout(self.plots_tab_layout)
+        self.plots_tab.setVerticalScrollBarPolicy(QtCore.Qt.ScrollBarPolicy.ScrollBarAsNeeded)
     
     def setup_settings_tab(self):
         print("This is the settings tab")
@@ -133,27 +144,27 @@ class TabWidget(PyQt5.QtWidgets.QWidget):
     
     def setup_group_table_widget(self):
         self.groups = self.get_groups()
-        self.group_table_widget = PyQt5.QtWidgets.QTableWidget(len(self.groups[1]), 3, self)
+        self.group_table_widget = QtWidgets.QTableWidget(len(self.groups[1]), 3, self)
         self.group_table_widget.setHorizontalHeaderLabels(['Group', '  Message Count  ', '  Group ID  '])
         i = 0
         for group in self.groups[1]:
-            self.group_table_widget.setItem(i, 0, PyQt5.QtWidgets.QTableWidgetItem(self.groups[2][group][0]))
-            self.group_table_widget.setItem(i, 1, PyQt5.QtWidgets.QTableWidgetItem(str(self.groups[2][group][1])))
-            self.group_table_widget.setItem(i, 2, PyQt5.QtWidgets.QTableWidgetItem(str(group)))
+            self.group_table_widget.setItem(i, 0, QtWidgets.QTableWidgetItem(self.groups[2][group][0]))
+            self.group_table_widget.setItem(i, 1, QtWidgets.QTableWidgetItem(str(self.groups[2][group][1])))
+            self.group_table_widget.setItem(i, 2, QtWidgets.QTableWidgetItem(str(group)))
             i += 1
         self.group_table_widget.itemSelectionChanged.connect(self.group_table_widget_update_selection)
-        self.group_table_widget.setSelectionBehavior(PyQt5.QtWidgets.QAbstractItemView.SelectRows)
-        self.group_table_widget.setEditTriggers(PyQt5.QtWidgets.QTableWidget.NoEditTriggers)
+        self.group_table_widget.setSelectionBehavior(QtWidgets.QAbstractItemView.SelectRows)
+        self.group_table_widget.setEditTriggers(QtWidgets.QTableWidget.NoEditTriggers)
         header = self.group_table_widget.horizontalHeader()
-        header.setSectionResizeMode(0, PyQt5.QtWidgets.QHeaderView.Stretch)
-        header.setSectionResizeMode(1, PyQt5.QtWidgets.QHeaderView.ResizeToContents)
-        header.setSectionResizeMode(2, PyQt5.QtWidgets.QHeaderView.ResizeToContents)
+        header.setSectionResizeMode(0, QtWidgets.QHeaderView.Stretch)
+        header.setSectionResizeMode(1, QtWidgets.QHeaderView.ResizeToContents)
+        header.setSectionResizeMode(2, QtWidgets.QHeaderView.ResizeToContents)
         self.group_tab_layout.addWidget(self.group_table_widget, 1, 0)
-        self.group_analysis_all_btn = PyQt5.QtWidgets.QPushButton('Analyze All', self)
-        self.group_analysis_selected_btn = PyQt5.QtWidgets.QPushButton('Analyze Selected', self)
+        self.group_analysis_all_btn = QtWidgets.QPushButton('Analyze All', self)
+        self.group_analysis_selected_btn = QtWidgets.QPushButton('Analyze Selected', self)
         self.group_analysis_all_btn.clicked.connect(self.retrieve_group_messages)
         self.group_analysis_selected_btn.clicked.connect(self.retrieve_group_messages)
-        group_tab_sublayout_2 = PyQt5.QtWidgets.QHBoxLayout()
+        group_tab_sublayout_2 = QtWidgets.QHBoxLayout()
         group_tab_sublayout_2.addStretch()
         group_tab_sublayout_2.addWidget(self.group_analysis_all_btn)
         group_tab_sublayout_2.addStretch()
@@ -163,27 +174,27 @@ class TabWidget(PyQt5.QtWidgets.QWidget):
     
     def setup_chat_table_widget(self):
         self.chats = self.get_chats()
-        self.chat_table_widget = PyQt5.QtWidgets.QTableWidget(len(self.chats[1]), 3, self)
+        self.chat_table_widget = QtWidgets.QTableWidget(len(self.chats[1]), 3, self)
         self.chat_table_widget.setHorizontalHeaderLabels(['Chat', '  Message Count  ', '  Chat ID  '])
         i = 0
         for chat in self.chats[1]:
-            self.chat_table_widget.setItem(i, 0, PyQt5.QtWidgets.QTableWidgetItem(self.chats[2][chat][0]))
-            self.chat_table_widget.setItem(i, 1, PyQt5.QtWidgets.QTableWidgetItem(str(self.chats[2][chat][1])))
-            self.chat_table_widget.setItem(i, 2, PyQt5.QtWidgets.QTableWidgetItem(str(chat)))
+            self.chat_table_widget.setItem(i, 0, QtWidgets.QTableWidgetItem(self.chats[2][chat][0]))
+            self.chat_table_widget.setItem(i, 1, QtWidgets.QTableWidgetItem(str(self.chats[2][chat][1])))
+            self.chat_table_widget.setItem(i, 2, QtWidgets.QTableWidgetItem(str(chat)))
             i += 1
         self.chat_table_widget.itemSelectionChanged.connect(self.chat_table_widget_update_selection)
-        self.chat_table_widget.setSelectionBehavior(PyQt5.QtWidgets.QAbstractItemView.SelectRows)
-        self.chat_table_widget.setEditTriggers(PyQt5.QtWidgets.QTableWidget.NoEditTriggers)
+        self.chat_table_widget.setSelectionBehavior(QtWidgets.QAbstractItemView.SelectRows)
+        self.chat_table_widget.setEditTriggers(QtWidgets.QTableWidget.NoEditTriggers)
         header = self.chat_table_widget.horizontalHeader()
-        header.setSectionResizeMode(0, PyQt5.QtWidgets.QHeaderView.Stretch)
-        header.setSectionResizeMode(1, PyQt5.QtWidgets.QHeaderView.ResizeToContents)
-        header.setSectionResizeMode(2, PyQt5.QtWidgets.QHeaderView.ResizeToContents)
+        header.setSectionResizeMode(0, QtWidgets.QHeaderView.Stretch)
+        header.setSectionResizeMode(1, QtWidgets.QHeaderView.ResizeToContents)
+        header.setSectionResizeMode(2, QtWidgets.QHeaderView.ResizeToContents)
         self.chat_tab_layout.addWidget(self.chat_table_widget, 1, 0)
-        self.chat_analysis_all_btn = PyQt5.QtWidgets.QPushButton('Analyze All', self)
-        self.chat_analysis_selected_btn = PyQt5.QtWidgets.QPushButton('Analyze Selected', self)
+        self.chat_analysis_all_btn = QtWidgets.QPushButton('Analyze All', self)
+        self.chat_analysis_selected_btn = QtWidgets.QPushButton('Analyze Selected', self)
         self.chat_analysis_all_btn.clicked.connect(self.retrieve_chat_messages)
         self.chat_analysis_selected_btn.clicked.connect(self.retrieve_chat_messages)
-        chat_tab_sublayout2 = PyQt5.QtWidgets.QHBoxLayout()
+        chat_tab_sublayout2 = QtWidgets.QHBoxLayout()
         chat_tab_sublayout2.addStretch()
         chat_tab_sublayout2.addWidget(self.chat_analysis_all_btn)
         chat_tab_sublayout2.addStretch()
@@ -214,15 +225,15 @@ class TabWidget(PyQt5.QtWidgets.QWidget):
                     self.received_group_msgs = GroupMeStats.retrieve_group_messages(self.selected_group_ids, self.groups[2])
                     print(self.selected_group_ids)
                 else:
-                    error_dialog = PyQt5.QtWidgets.QMessageBox()
-                    error_dialog.setIcon(PyQt5.QtWidgets.QMessageBox.Critical)
+                    error_dialog = QtWidgets.QMessageBox()
+                    error_dialog.setIcon(QtWidgets.QMessageBox.Critical)
                     error_dialog.setText("Error")
                     error_dialog.setInformativeText("Some error occurred. Expecting at least one group selected.")
                     error_dialog.setWindowTitle("Error")
                     error_dialog.exec_()
             except AttributeError:
-                warning_dialog = PyQt5.QtWidgets.QMessageBox()
-                warning_dialog.setIcon(PyQt5.QtWidgets.QMessageBox.Warning)
+                warning_dialog = QtWidgets.QMessageBox()
+                warning_dialog.setIcon(QtWidgets.QMessageBox.Warning)
                 warning_dialog.setText("Warning")
                 warning_dialog.setInformativeText("You should select at least one group to use this function.")
                 warning_dialog.setWindowTitle("Warning")
@@ -231,10 +242,24 @@ class TabWidget(PyQt5.QtWidgets.QWidget):
             return
     
         total_stats = {}
-        group_stats = GroupMeStats.determine_user_statistics(self.received_group_msgs, self.selected_group_ids, 'group', self.users)
+        users_subset = set()
+        users_list = []
+        for group_id in self.selected_group_ids:
+            for group in self.groups[0]:
+                if group["group_id"] == group_id:
+                    for member in group["members"]:
+                        users_list.append(member["user_id"])
+        
+        current_user_id = GroupMeStats.get_me()["response"]["id"]
+        if current_user_id not in users_list:
+            users_list.append(current_user_id)
+
+        users_subset.update(users_list)
+        group_stats = GroupMeStats.determine_user_statistics(self.received_group_msgs, self.selected_group_ids, 'group', users_subset)
         total_stats.update(group_stats[0])
         num_messages = group_stats[1]
         GroupMeStats.write_to_csv(total_stats, num_messages)
+        self.plot_stats(total_stats)
     
     def retrieve_chat_messages(self):
         if self.chat_analysis_all_btn == self.sender():
@@ -243,22 +268,22 @@ class TabWidget(PyQt5.QtWidgets.QWidget):
         elif self.chat_analysis_selected_btn == self.sender():
             try:
                 if len(self.selected_chat_ids) > 0:
-                    # message_dialog = PyQt5.QtWidgets.QMessageBox()
+                    # message_dialog = QtWidgets.QMessageBox()
                     # message_dialog.setText("Please Wait")
                     # message_dialog.setInformativeText("Retrieving messages")
                     # message_dialog.show()
                     self.received_chat_msgs = GroupMeStats.retrieve_chat_messages(self.selected_chat_ids, self.chats[2], from_gui = True)
                     print(self.selected_chat_ids)
                 else:
-                    error_dialog = PyQt5.QtWidgets.QMessageBox()
-                    error_dialog.setIcon(PyQt5.QtWidgets.QMessageBox.Critical)
+                    error_dialog = QtWidgets.QMessageBox()
+                    error_dialog.setIcon(QtWidgets.QMessageBox.Critical)
                     error_dialog.setText("Error")
                     error_dialog.setInformativeText("Some error occurred. Expecting at least one chat selected.")
                     error_dialog.setWindowTitle("Error")
                     error_dialog.exec_()
             except AttributeError:
-                warning_dialog = PyQt5.QtWidgets.QMessageBox()
-                warning_dialog.setIcon(PyQt5.QtWidgets.QMessageBox.Warning)
+                warning_dialog = QtWidgets.QMessageBox()
+                warning_dialog.setIcon(QtWidgets.QMessageBox.Warning)
                 warning_dialog.setText("Warning")
                 warning_dialog.setInformativeText("You should select at least one chat to use this function.")
                 warning_dialog.setWindowTitle("Warning")
@@ -267,13 +292,20 @@ class TabWidget(PyQt5.QtWidgets.QWidget):
             return
 
         total_stats = {}
-        chat_stats = GroupMeStats.determine_user_statistics(self.received_chat_msgs, self.selected_chat_ids, 'direct', self.users)
+        users_subset = set()
+        users_list = []
+        current_user_id = GroupMeStats.get_me()["response"]["id"]
+        users_list.append(current_user_id)
+        for chat_id in self.selected_chat_ids:
+            users_list.append(chat_id)
+        users_subset.update(users_list)
+        chat_stats = GroupMeStats.determine_user_statistics(self.received_chat_msgs, self.selected_chat_ids, 'direct', users_subset)
         total_stats.update(chat_stats[0])
         num_messages = chat_stats[1]
         GroupMeStats.write_to_csv(total_stats, num_messages)
-        self.plot_chat_messages(total_stats)
+        self.plot_stats(total_stats)
     
-    def plot_chat_messages(self, stats):
+    def plot_stats(self, stats):
         x_axis = []
         messages_sent_y_axis, likes_received_y_axis, likes_given_y_axis, self_likes_y_axis, words_sent_y_axis, images_sent_y_axis = ([] for i in range(6))
         for key, value in stats.items():
@@ -302,12 +334,12 @@ class TabWidget(PyQt5.QtWidgets.QWidget):
         self.images_sent_graph.plot_graph()
 
 
-class OutputWindow(PyQt5.QtWidgets.QPlainTextEdit):
+class OutputWindow(QtWidgets.QPlainTextEdit):
     def write(self, text):
         self.appendPlainText("str(text)")
         self.update()
 
 if __name__ == '__main__':
-    app = PyQt5.QtWidgets.QApplication(sys.argv)
+    app = QtWidgets.QApplication(sys.argv)
     gui = GUI()
     sys.exit(app.exec_())
